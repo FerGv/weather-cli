3d0186abc05c85482981518c8c89358a4929cb02
Fernando Garcia <jfer.garciav@gmail.com>
2021-01-20 23:40:22 -0600

    [Index] Show title as ASCII text

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
 // Libraries
 import chalk from 'chalk';
+import clear from 'clear';
 
 // API
 import api from './api';
+
+// Utils
+import { asciiText } from './utils';
 
 /**
  * Run main CLI process.
  */
 async function main() {
-  console.log(chalk.cyan('Weather API'));
+  clear();
+  const title = await asciiText('Weather CLI');
+  console.log(chalk.cyan(title));
   await api.getCurrentWeather('Mexico City');
 }
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,6 @@
 // Libraries
 import chalk from 'chalk';
+import figlet from 'figlet';
 
 /**
  * Log an error into console.
@@ -8,4 +9,19 @@ import chalk from 'chalk';
  */
 export function logError(error: string): void {
   console.log(chalk.red(error));
+}
+
+/**
+ * Transform a text into its ASCII representation.
+ *
+ * @param text - The text to transform.
+ */
+export function asciiText(text: string): Promise<string> {
+  return new Promise((resolve) => {
+    figlet(text, (err, data = '') => {
+      if (err) resolve(text);
+
+      resolve(data);
+    });
+  });
 }
